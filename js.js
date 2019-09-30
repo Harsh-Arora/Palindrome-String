36cda4b8ca21eb84b35c3fd91b5782fb3074dbf2
Harsh Arora <harsh.arora1821@gmail.com>
2019-09-30 23:29:04 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+function action(){
+	var str = document.getElementById('input').value;
+	var rev = "";
+	// alert(str.length);
+
+	for(var i=str.length-1;i>=0;i--){
+		rev = rev.concat(str.charAt(i));
+	}
+
+	if(rev===str){
+		document.getElementById('res').innerHTML = "'"+str+"' is a palindrome word/phrase."
+	}
+	else{
+		document.getElementById('res').innerHTML = "'"+str+"' is not a palindrome word/phrase."
+	}
+
+
+	// alert(str);
+}
